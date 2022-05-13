6bb8545faa7759748a2f1b4a48276ff20175c1a2
jey1322 <jeypaniagua@gmail.com>
2022-05-12 23:18:38 -0600

    con mismo error, api docente y curso, con el objetivo de relacionar con api grupo

---
+++ app/Http/Resources/V1/DocenteResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DocenteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return[
+            'id' => $this->id,
+            'carnet' => $this->carnet,
+            'nombre' => $this->nombre,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\MatriculaController as MatriculaV1;
 use App\Http\Controllers\Api\V1\PromotorController as PromotorV1;
 use App\Http\Controllers\Api\V1\GrupoController as GrupoV1;
+use App\Http\Controllers\Api\V1\CursoController as CursoV1;
+use App\Http\Controllers\Api\V1\DocenteController as DocenteV1;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -29,6 +31,14 @@ Route::apiResource('v1/promotores', PromotorV1::class)
         ->middleware('auth:sanctum');
 
 Route::apiResource('v1/grupos', GrupoV1::class)
+        ->only(['index', 'show'])
+        ->middleware('auth:sanctum');
+
+Route::apiResource('v1/cursos', CursoV1::class)
+        ->only(['index', 'show'])
+        ->middleware('auth:sanctum');
+
+Route::apiResource('v1/docentes', DocenteV1::class)	
         ->only(['index', 'show'])
         ->middleware('auth:sanctum');
 
