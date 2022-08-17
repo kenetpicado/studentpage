826aaeba5e081e52cf14d5b892a5abe8808e898c
Kenet Picado <kenetpicado1@gmail.com>
2022-08-17 01:08:33 -0600

    recibo

--- resources/views/pago/recibo.blade.php
+++ resources/views/pago/recibo.blade.php
@@ -5,47 +5,79 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="{{ asset('img/SP.png') }}">
-    <title>{{ config('app.name') }} | Recibo</title>
+    <title>{{ config('app.name') }} - Recibo</title>
     <link href="{{ asset('css/app2.css') }}" rel="stylesheet">
 
     <style>
         .page {
-            width: 21.6cm;
-            min-height: 9.3cm;
-            margin: 2cm auto;
-            background: white;
+            width: 21.7cm;
+            height: 9.3cm;
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
         }
 
         @page {
-            size: 21.6cm 9.3cm;
+            size: 21.7cm 9.3cm;
             margin: 0;
         }
 
         @media print {
-            .page {
-                margin: 0;
-                border: initial;
-                border-radius: initial;
-                width: initial;
-                min-height: initial;
-                background: initial;
-                page-break-after: always;
+            .ocultar,
+            .ocultar * {
+                display: none !important;
             }
         }
     </style>
-
 </head>
 
-<body class="bg-primary">
-    <div class="page">
-        <table style="padding-left: 2cm;" width="100%">
-            <tr class="bg-dark">
-                <td width="50%">a</td>
-                <td>a</td>
-                <td>a</td>
-            </tr>
-        </table>
+<body class="bg-light">
+    <div class="page" style="background-image: url({{ asset('img/gaita.jpg') }});">
+        <div class="card bg-transparent border-0 m-0" style="height: 100%;">
+            <div class="row" style="margin-top: 2.85cm; margin-left: 2.4cm;">
+                <table>
+                    <tr>
+                        <td width="62%">{{ $pago->created_at }}</td>
+                        <td width="23%">{{ $pago->moneda == 'CORDOBAS' ? $pago->monto : '' }}</td>
+                        <td>{{ $pago->moneda == 'DOLARES' ? $pago->monto : '' }}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="row" style="margin-top: 2px; margin-left: 3cm;">
+                <td>{{ auth()->user()->name }}</td>
+            </div>
+            <div class="row" style="margin-top: 0.2cm; margin-left: 4cm;">
+                <td>{{ $pago->monto }} {{ $pago->moneda }}</td>
+            </div>
+            <div class="row" style="margin-top: 0.2cm; margin-left: 4cm;">
+                <td>{{ $pago->concepto }}</td>
+            </div>
+            <div class="row" style="margin-top: 0.2cm; margin-left: 12cm;">
+                <td>
+                    @if ($pago->saldo)
+                        {{ $pago->saldo }} {{ $pago->moneda }}
+                    @else
+                        ---
+                    @endif
+                </td>
+            </div>
+            <div class="row" style="margin-top: 0.2cm; margin-left: 10.4cm;">
+                {{ $pago->curso }}
+            </div>
+            <div class="row" style="margin-top: 0.2cm; margin-left: 2.5cm;">
+                <table>
+                    <tr>
+                        <td width="31%">{{ $pago->horario }}</td>
+                        <td>{{ $pago->docente }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
+    <p class="text-center mt-5">
+        <a href="javascript:window.print()" class="btn btn-primary ocultar float-center">
+            Imprimir
+        </a>
+    </p>
 </body>
 
 </html>
